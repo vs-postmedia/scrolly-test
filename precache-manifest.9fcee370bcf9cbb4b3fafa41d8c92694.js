f5340b1de20daa615a8ca86689fece4e09fc4438
Nathan Griffiths <ngriffiths@postmedia.com>
2019-11-18 12:53:28 -0800

    Updates

---
+++ precache-manifest.9fcee370bcf9cbb4b3fafa41d8c92694.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db8bcf20709805571b9620837e02461b",
+    "url": "/scrolly-test/index.html"
+  },
+  {
+    "revision": "f91bd92ad08a6110ef42",
+    "url": "/scrolly-test/static/css/main.a03dbb20.chunk.css"
+  },
+  {
+    "revision": "19d50e7613a989a19b3f",
+    "url": "/scrolly-test/static/js/2.2ce84108.chunk.js"
+  },
+  {
+    "revision": "f91bd92ad08a6110ef42",
+    "url": "/scrolly-test/static/js/main.88fc2107.chunk.js"
+  },
+  {
+    "revision": "0e9f21c5e9601945392b",
+    "url": "/scrolly-test/static/js/runtime~main.4fd5de05.js"
+  },
+  {
+    "revision": "6f6753fd733a216b24786a168657540a",
+    "url": "/scrolly-test/static/media/BentonSansCond-Bold.6f6753fd.otf"
+  },
+  {
+    "revision": "a6640d8951e3d6c523b2b58b0db9e351",
+    "url": "/scrolly-test/static/media/BentonSansCond-BoldItalic.a6640d89.otf"
+  },
+  {
+    "revision": "69a4217c7da7462a2511b1a8ecaefecf",
+    "url": "/scrolly-test/static/media/BentonSansCond-RegItalic.69a4217c.otf"
+  },
+  {
+    "revision": "43cd4ac75abf53e257d21366d3322b0e",
+    "url": "/scrolly-test/static/media/BentonSansCond-Regular.43cd4ac7.otf"
+  },
+  {
+    "revision": "cdd38a860699d029f95e76f21e51e216",
+    "url": "/scrolly-test/static/media/Shift-Bold.cdd38a86.otf"
+  },
+  {
+    "revision": "5c3edbfdeedc59bb35fac3bf1a461926",
+    "url": "/scrolly-test/static/media/Shift-BoldItalic.5c3edbfd.otf"
+  }
+]);
